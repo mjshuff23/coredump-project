5261241569d612312e01f3acf8daa200792e2ea0
john <jgh2102@gmail.com>
2020-10-06 03:24:57 +0900

    added initialized sequelize database

---
+++ express-sequelize-coredump/db/migrations/20201004053634-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userName: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      bio: {
+        type: Sequelize.TEXT
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
