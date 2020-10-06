5ef7bfe3a91178b545cf71fad89a2c265249e937
Michael Shuff <mjshuff23@gmail.com>
2020-10-06 14:46:50 -0400

    finished answers

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const { searchRouter }  = require('./routes/search');
 
 const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/search', searchRouter);
 
 app.get('/', (req, res) => {
   res.render('site-layout');
@@ -28,57 +30,6 @@ app.get('/login', (req, res) => {
 app.get('/signup', (req, res) => {
   res.render('signup');
 })
-
-let answers = [
-  {
-    questionId: 2,
-    answerText: "A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).",
-    userId: 1
-  },
-  {
-    questionId: 1,
-    answerText: "Switchable help-desks are not a real thing.",
-    userId: 2
-  },
-];
-
-app.post('/search', asyncHandler(async(req, res, next) => {
-  let { search } = req.body;         // Grab search value
-  let relevantQuestions = [];
-  let associatedAnswers = [];
-
-  search = search.toLowerCase();                              // Make it lowercase
-  const questions = await Question.findAll();
-  // Search through questions list to find relevant questions
-  for (let i = 0; i < questions.length; i++) {
-    let questionText = questions[i].questionText.toLowerCase();
-    let questionSubject = questions[i].questionSubject.toLowerCase();
-
-    if (questionText.includes(` ${search}`) || questionSubject.includes(` ${search}`)
-     || questionText.startsWith(search)      || questionSubject.startsWith(search)) {
-      relevantQuestions.push(questions[i]);
-    }
-  }
-
-  if (relevantQuestions.length === 0) {
-    relevantQuestions[0] = {
-      questionSubject: `No results found`,
-      questionText: `We couldn't find any results for your search! Try again!`
-    };
-  } else {
-    // Loop through questions and find associated answers
-    for (let i = 0; i < relevantQuestions.length; i++) {
-      let questionId = relevantQuestions[i].id;
-      let answers = await Answer.findAll({
-        where: {
-          questionId: questionId,
-        },
-      })
-      relevantQuestions[i].answers = answers;
-    }
-  }
-  res.render('search', { relevantQuestions });
-}));
 
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
---
+++ routes/search.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const db = require('../db/models');
+const { User, Question, Answer, Vote } = db;
+const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { Op } = require("sequelize");
+const router = express.Router();
+
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
+
+router.post('/', asyncHandler(async(req, res, next) => {
+  let { search } = req.body;         // Grab search value
+  let relevantQuestions = [];
+  let questionIds = [];
+  let answers = [];
+
+  search = search.toLowerCase();                              // Make it lowercase
+  const questions = await Question.findAll();
+  // Search through questions list to find relevant questions
+  for (let i = 0; i < questions.length; i++) {
+    let questionText = questions[i].questionText.toLowerCase();
+    let questionSubject = questions[i].questionSubject.toLowerCase();
+
+    if (questionText.includes(` ${search}`) || questionSubject.includes(` ${search}`)
+     || questionText.startsWith(search)      || questionSubject.startsWith(search)) {
+      relevantQuestions.push(questions[i]);
+      questionIds.push(questions[i].id);
+    }
+  }
+
+  if (relevantQuestions.length === 0) {
+    relevantQuestions[0] = {
+      questionSubject: `No results found`,
+      questionText: `We couldn't find any results for your search! Try again!`
+    };
+  } else {
+    console.log(questionIds)
+    // Loop through questions and find associated answers
+    answers = await Answer.findAll({
+      where: {
+        questionId: {
+          [Op.in]: questionIds,
+        }
+      },
+    });
+    relevantQuestions = relevantQuestions.map(question => {
+      question.answers = [];
+      for (let i = 0; i < answers.length; i++) {
+        if (question.id === answers[i].questionId) {
+          question.answers.push(answers[i]);
+        }
+      }
+      return question;
+    });
+    console.log(answers);
+  }
+  res.render('search', { relevantQuestions });
+}));
+
+
+module.exports = {
+  searchRouter: router,
+}
