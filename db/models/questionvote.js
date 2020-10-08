93fdc44338b4d4c8418101408a7b90024f72032a
marysue <mary@marylark.com>
2020-10-08 14:03:20 -0600

    Added questions and answer vote db tables, removed vote table

---
+++ db/models/questionvote.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const QuestionVote = sequelize.define('QuestionVote', {
+    vote: DataTypes.BOOLEAN,
+    userId: DataTypes.INTEGER,
+    questionId: DataTypes.INTEGER
+  }, {});
+  QuestionVote.associate = function(models) {
+    // associations can be defined here
+    QuestionVote.belongsTo( models.Question, { foreignKey: 'questionId' });
+    QuestionVote.belongsTo( models.User, { foreignKey: 'userId' });
+  };
+  return QuestionVote;
+};
