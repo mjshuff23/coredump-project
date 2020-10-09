052545bcf31e869e88e44d51b3a7d37a31214cf9
marysue <mary@marylark.com>
2020-10-09 17:47:45 -0600

    Most coding is done for votes, but untested.

--- public/js/votes.js
+++ public/js/votes.js
@@ -1,4 +1,131 @@
 document.addEventListener('DOMContentLoaded', (e) => {
 
-    
+    document.querySelector('.question.fa-caret-up').addEventListener( 'click', (e) => {
+        const questionElt = document.querySelector(`[data-questionid]`);
+        const questionId = questionElt.dataset.questionid;
+        const userId = localStorage.getItem("COREDUMP_CURRENT_USER_ID");
+        const route = "/questions/voteQuestion";
+        const body = { vote: true, userId, questionId };
+        let res = postVote(body, route);
+        if (res.ok) {
+            let score = questionElt.querySelector('.score');
+            let newScore = parseInt(score.innerHTML);
+            newScore -= 1;
+            score.innerHTML = newScore;
+        } else {
+            setErrorMessage(res);
+        }
+    });
+
+    document.querySelector('.question.fa-caret-down').addEventListener('click', (e) => {
+        const questionElt = document.querySelector(`[data-questionid]`);
+        const questionId = questionElt.dataset.questionid;
+        const userId = localStorage.getItem("COREDUMP_CURRENT_USER_ID");
+        const route = "/questions/voteQuestion";
+        const body = { vote: false, userId, questionId };
+        let res = postVote(body, route);
+        if (res.ok) {
+            let score = questionElt.querySelector('.score');
+            let newScore = parseInt(score.innerHTML);
+            newScore -= 1;
+            score.innerHTML = newScore;
+        } else {
+            setErrorMessage(res);
+        }
+    });
+
+    document.querySelector('.questionAnswer.fa-caret-up').addEventListener('click', (e) => {
+        const answerElt = document.querySelector(`[data-answerid]`);
+        const answerId = answerElt.dataset.questionid;
+        const questionElt = document.querySelector(`[data-questionid]`);
+        const questionId = questionElt.dataset.questionid;
+        const userId = localStorage.getItem("COREDUMP_CURRENT_USER_ID");
+        route = "/questions/voteAnswer";
+        const body = { vote: true, userId, answerId };
+        let res = postVote(body, route);
+        if (res.ok) {
+            let score = answerElt.querySelector('.score');
+            let newScore = parseInt(score.innerHTML);
+            newScore += 1;
+            score.innerHTML = newScore
+        } else {
+          setErrorMessage(res);
+        }
+    });
+
+    document.querySelector('.questionAnswer.fa-caret-down').addEventListener('click', (e) => {
+        const  answerElt = document.querySelector(`[data-answerid]`);
+        const  answerId = answerElt.dataset.questionid;
+        const  questionElt = document.querySelector(`[data-questionid]`);
+        const  questionId = questionElt.dataset.questionid;
+        const  userId = localStorage.getItem("COREDUMP_CURRENT_USER_ID");
+        const route = "/questions/voteAnswer";
+        const body = { vote: false, userId, answerId };
+        let res = postVote(body, route);
+        //look at results and if ok, then change p.score increasing or decreasing.
+        if (res.ok) {
+            let score = answerElt.querySelector('.score');
+            let newScore = parseInt(score.innerHTML);
+            newScore -= 1;
+            score.innerHTML = newScore
+        } else {
+            setErrorMessage(res);
+        }
+    });
+});
+
+async function setErrorMessage(res) {
+    const errorJSON = await res.json();
+    const { errors } = errorJSON;
+
+    if (errors && Array.isArray(errors)) {
+        let errorsContainer = document.querySelector(".main-box");
+        errorsContainer.innerHTML = ``;
+        let errorsHTML = ``;
+        for (let i = 0; i < errors.length; i++)
+        {
+            errorsHTML += `<li>${errors[i]}</li>`;
+        }
+        errorsContainer.innerHTML = `<ul style="color:red; background-color:white">${errorsHTML}</ul>`;
+
+        await setTimeout(function() {errorsContainer.innerHTML = `<ul style="color:red; background-color:white">${errorsHTML}</ul>`;}, 5000);
+        errorsContainer.innerHTML = ``;
+    }
+}
+
+async function postVote(body,route) {
+    try {
+        const res = await fetch(route, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!res.ok) {
+            throw res;
+        }
+        return res;
+    }
+    catch (res)  {
+        console.log("err:  ", res.status);
+        if (res.status >= 400 && res.status < 600) {
+          const errorJSON = await res.json();
+          const { errors } = errorJSON;
+
+          //here display for 5 seconds the error message in the pug file
+            //create a div, position absolute, put at top of page, then remove after timeout.
+
+          alert("Received between a 400 and 600 error.  Need to finish error handling in votes.js");
+          //questionSubject = body.questionSubject;
+          //questionText = body.questionText;
+
+          if (errors && Array.isArray(errors)) {
+            alert("We need to process the errors array:  ", errors);
+          }
+        } else {
+            alert("Something went wrong. Please check your internet connection and try again!");
+        }
+    }
 }
--- routes/api/questions.js
+++ routes/api/questions.js
@@ -21,14 +21,14 @@ const validateQuestion = [
   handleValidationErrors,
 ];
 
-async function registerAnswerVote(vote, userId, answerId) {
+async function registerAnswerVote(vote, userId, answerId, res) {
     let voted = await AnswerVote.findAll( {
         where: {
             userId,
             answerId
         }
     })
-    if (!setVote(voted, vote)) {
+    if (!setVote(voted, vote, res)) {
         await AnswerVote.create( {
             vote,
             userId,
@@ -45,7 +45,7 @@ async function registerQuestionVote(vote, userId, questionId) {
         }
     });
 
-   if (!setVote(voted, vote)){
+   if (!setVote(voted, vote, res)){
        await QuestionVote.create( {
            vote,
            userId,
@@ -86,17 +86,17 @@ function countVotes(votes) {
   }
   return score;
 }
-function setVote(voted, vote) {
+function setVote(voted, vote, res) {
   //if we find a vote for this userId and AnswerId, let's see if we can change it.
    let retVal = true;
    if (voted) {
         //up vote (true) - cannot upvote
         if (vote) {
             //current vote is false; We can only set it if the current vote is true;
-           voted.vote ? console.log("Cannot upvote an upvote") : voted.vote=vote;
+           voted.vote ? res.json( { errors: "Cannot upvote an upvote" }) : voted.vote=vote; //display to user already voted in res.json -- display error timeout 5 seconds on pug page
         }
         else if(!vote) { //down vote (false)
-            voted.vote ? voted.vote=vote : console.log("Cannot downvote a downvote");
+            voted.vote ? voted.vote=vote : res.json( {errors: "Cannot downvote a downvote"}); //display to user already voted in res.json -- display error timeout 5 seconds on pug page
         }
         return retVal;
     }
@@ -114,10 +114,22 @@ router.post(
 );
 
 router.post(
-    '/upVoteQuestion',
+    '/voteQuestion',
     (req, res) => {
-        console.log("Hit the post question route!");
+        console.log("Hit the upVoteQuestion route!");
+        const { vote, userId, questionId } = req.body;
+        registerQuestionVote(vote, userId, questionId, res);
+        //return res.json with error
     });
+
+router.post(
+    '/voteAnswer',
+    (req, res) => {
+        console.log("Hit the upVoteAnswer route...");
+        const { vote, userId, answerId } = req.body;
+        registerAnswerVote(vote, userId, answerId, res);
+    });
+
 
 router.get('/', asyncHandler(async(req, res, next) => {
     // Get all questions
