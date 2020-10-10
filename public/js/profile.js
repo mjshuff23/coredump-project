0d13ee8267a8b19fb325b1e5eb64d1ea4835e023
john <jgh2102@gmail.com>
2020-10-11 02:42:29 +0900

    starting public js

---
+++ public/js/profile.js
@@ -0,0 +1,46 @@
+const handleDelete = (userId) => {
+    return async () => {
+      try {
+        const res = await fetch(`/users/${userId}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem(
+              "COREDUMP_ACCESS_TOKEN"
+            )}`,
+          },
+        });
+        if (!res.ok) {
+          throw res;
+        }
+        document.querySelector(`#userDelete-${userId}`).remove();
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  };
+
+  document.addEventListener("DOMContentLoaded", async () => {
+    const userId = localStorage.getItem("COREDUMP_CURRENT_USER_ID");
+    try {
+      const res = await fetch(`/users/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(
+            "COREDUMP_ACCESS_TOKEN"
+          )}`,
+        },
+      });
+      if (res.status === 401) {
+        window.location.href = "/log-in";
+        return;
+      }
+  
+      const deleteButtons = document.querySelectorAll(".userDelete-button");
+      if (deleteButtons) {
+        deleteButtons.forEach((button) => {
+          button.addEventListener("click", handleDelete(button.id));
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
